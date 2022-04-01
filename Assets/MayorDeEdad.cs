795c5419162a2259ad130989bbd5e726c709573c
BautyGarcia <bautistaroccogarcia@gmail.com>
2022-04-01 11:56:56 -0300

    Vid 4 and below done
    nice

---
+++ Assets/MayorDeEdad.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MayorDeEdad : MonoBehaviour
+{
+    public int Edad;
+    public Text myText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Edad >= 18){
+            myText.text = "Es mayor de edad";
+        }else{
+            myText.text = "No es mayor de edad";
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
